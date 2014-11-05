c6e6fd2d16be37437d1aa191a215d83145fc0d86
Ismael G Marin C <ismael.marin@gmail.com>
2014-11-05 13:03:05 -0600

    Base for user creation with roles config and views

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  rolify
+  devise :database_authenticatable, :registerable, #:omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :default_role
+
+  private
+  def default_role
+    self.add_role :usuario
+  end
+end
