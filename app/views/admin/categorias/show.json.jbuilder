8bbe332d24d61fd2437260499e85682937f83a1b
Ismael G Marin C <ismael.marin@gmail.com>
2014-11-06 12:12:58 -0600

    Base for categorias with admin

---
+++ app/views/admin/categorias/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @categoria, :id, :titulo, :slug, :prioridad, :visible, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :categorias
+  resources :categorias, only: [:show, :index]
+
+  namespace :admin do
+    resources :categorias
+  end
 
   devise_for :users
   root 'home#index'
