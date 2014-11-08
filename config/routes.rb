5db281a3cc23074f12d6325375410507773c94b6
Gize Bonilla <tiliches88@gmail.com>
2014-11-08 00:34:08 -0600

    base admin & categories index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   resources :categorias, only: [:show, :index]
+  resources :admin, only: [:index]
 
   namespace :admin do
     resources :categorias
   end
 
   devise_for :users
-  root 'home#index'
+
+  get 'admin', to: 'admin#index', as: :admin
+
+  get 'examples', to: 'home#index', as: :examples
+
+  root 'categorias#index'
 end
