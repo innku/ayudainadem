ce0d55a524f11a193a92b1f287d26fa09a8a061e
Ismael G Marin C <ismael.marin@gmail.com>
2014-11-26 12:11:59 -0600

    Working on testing

--- spec/factories/articulos.rb
+++ spec/factories/articulos.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :articulo do
     titulo "MyString"
-contenido "MyString"
-subcategoria "MyString"
-video "MyString"
-imagen "MyString"
-slug "MyString"
+    contenido "MyString"
+    subcategoria "MyString"
+    video "MyString"
+    imagen "MyString"
+    slug "MyString"
   end
-
 end
