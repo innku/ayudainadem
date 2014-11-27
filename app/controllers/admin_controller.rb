e0c03d02ad95f92890fb8a55d1cac4dc2810886a
Ismael G Marin C <ismael.marin@gmail.com>
2014-11-27 12:17:59 -0600

    Verify admin to enter admin namespace

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,16 @@
 class AdminController < ApplicationController
-  before_filter :authenticate_user!
   layout 'admin'
+  before_filter :authenticate_user!
+  before_filter :verify_admin
+
   def index
     @categorias = Categoria.all
     @subcategorias = Subcategoria.all
     @articulos = Articulo.all
+  end
+
+  private
+  def verify_admin
+    redirect_to root_url unless current_user.has_role? :admin
   end
 end
